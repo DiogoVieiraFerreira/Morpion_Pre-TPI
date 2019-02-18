961fcb4f846d91a88361659c8199cdec5e7a2c09
Diogo Vieira <diogof-6@hotmail.com>
2019-02-18 15:26:41 +0100

    ajout de menu en haut du form view

--- CODE/Morpion/Morpion/Model.cs
+++ CODE/Morpion/Morpion/Model.cs
@@ -1,16 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+
 
 namespace Morpion
 {
     /// <summary>
     /// 
     /// </summary>
-    class Model
+    class Model 
     {
         DataBase db = new DataBase("Morpion");
+
+
+        public void localSolo_click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
