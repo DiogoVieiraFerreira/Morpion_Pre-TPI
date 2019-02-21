442d1460fbc3523b91b03cba49412faedf7e90b1
Diogo Vieira <diogof-6@hotmail.com>
2019-02-21 14:23:10 +0100

    création de l'interface de jeu

--- CODE/Morpion/Morpion/Model.cs
+++ CODE/Morpion/Morpion/Model.cs
@@ -20,35 +20,69 @@ namespace Morpion
         private View _view;
         private View _msgBox;
         private Size _viewSize;
+        private int _menu;
+        private int _score;
         private string _player01;
         private string _player02;
-        
-        private void LocalSolo_click(object sender, EventArgs e)
+        private bool _multi;
+        public void LocalSolo_click(object sender, EventArgs e)
         {
+            askUserName(1);
+        }
+
+        public void LocalMulti_click(object sender, EventArgs e)
+        {
+            askUserName(2);
+        }
+
+        private void askUserName(int oneOrTwoPlayers)
+        {
+            _multi = false;
             _msgBox = new View("Infos");
             _msgBox.Size = new Size(200, 150);
+
+
+            Label lblInfo01 = new Label();
+            lblInfo01.Text = "votre nom:";
+            lblInfo01.Name = "lblInfo";
+            lblInfo01.Location = new Point(15, 5);
+            _msgBox.Controls.Add(lblInfo01);
+
+            TextBox txtPlayer01 = new TextBox();
+            txtPlayer01.Name = "txtPlayer01";
+            txtPlayer01.Size = new Size(_msgBox.Size.Width - 40, 20);
+            txtPlayer01.Location = new Point(15, lblInfo01.Location.Y + lblInfo01.Size.Height);
+            _msgBox.Controls.Add(txtPlayer01);
             
-
-            Label lblInfo = new Label();
-            lblInfo.Text = "votre nom:";
-            lblInfo.Name = "lblInfo";
-            lblInfo.Location = new Point(15, 5);
-            _msgBox.Controls.Add(lblInfo);
-
-            TextBox txtUserName = new TextBox();
-            txtUserName.Name = "txtUserName";
-            txtUserName.Size = new Size(_msgBox.Size.Width - 40, 20);
-            txtUserName.Location = new Point(15, lblInfo.Location.Y+lblInfo.Size.Height);
-            _msgBox.Controls.Add(txtUserName);
-
             Button cmdOk = new Button();
             cmdOk.Name = "cmdOk";
             cmdOk.Text = "Ok";
             cmdOk.AutoSize = true;
-            cmdOk.Location = new Point(txtUserName.Location.X+txtUserName.Size.Width-cmdOk.Size.Width, txtUserName.Location.Y+txtUserName.Size.Height+5);
+            cmdOk.Location = new Point(txtPlayer01.Location.X + txtPlayer01.Size.Width - cmdOk.Size.Width, txtPlayer01.Location.Y + txtPlayer01.Size.Height + 5);
             cmdOk.Click += CmdOk_Click;
-
             _msgBox.Controls.Add(cmdOk);
+
+            
+            if (oneOrTwoPlayers == 2)
+            {
+                _multi = true;
+                _msgBox.Size = new Size(200, 170);
+
+                Label lblInfo02 = new Label();
+                lblInfo02.Text = "nom joueur 02:";
+                lblInfo02.Name = "lblInfo02";
+                lblInfo02.Location = new Point(15, txtPlayer01.Location.Y + txtPlayer01.Size.Height);
+                _msgBox.Controls.Add(lblInfo02);
+
+                TextBox txtPlayer02 = new TextBox();
+                txtPlayer02.Name = "txtPlayer02";
+                txtPlayer02.Size = new Size(_msgBox.Size.Width - 40, 20);
+                txtPlayer02.Location = new Point(15, lblInfo02.Location.Y + lblInfo02.Size.Height);
+                _msgBox.Controls.Add(txtPlayer02);
+
+
+                cmdOk.Location = new Point(txtPlayer02.Location.X + txtPlayer02.Size.Width - cmdOk.Size.Width, txtPlayer02.Location.Y + txtPlayer02.Size.Height + 5);
+            }
 
             _msgBox.Show();
             
@@ -57,104 +91,115 @@ namespace Morpion
 
         private void CmdOk_Click(object sender, EventArgs e)
         {
-            //je dois check si txtUserName.text est vide, mais j'arrive pas à récup son contenu :/ et je sais pas l'envoyer
-            if (_msgBox.Controls.Find("txtUserName", true)[0].Text == "")
+            if(_multi)
             {
-                MessageBox.Show("Merci de rentrer un nom d'utilisateur", "Nom utilisateur manquant", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                if(_msgBox.Controls.Find("txtPlayer01", true)[0].Text == "" || _msgBox.Controls.Find("txtPlayer02", true)[0].Text == "" || _msgBox.Controls.Find("txtPlayer01", true)[0].Text.Length> 11 || _msgBox.Controls.Find("txtPlayer02", true)[0].Text.Length > 11)
+                        MessageBox.Show("Merci de rentrer un nom d'utilisateur ayant au maximum 11 lettres", "Nom utilisateur manquant", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                else
+                {
+                    _player01 = _msgBox.Controls.Find("txtPlayer01", true)[0].Text;
+                    _player02 = _msgBox.Controls.Find("txtPlayer02", true)[0].Text;
+                    _msgBox.Close();
+                    _view.Text = "Morpion: Bienvenue " + _player01 + " et " + _player02;
+                }
             }
             else
             {
-                _player01 = _msgBox.Controls.Find("txtUserName", true)[0].Text;
-                _player02 = "Ordinateur";
-                _msgBox.Close();
-                _view.Text += ": Bienvenue " + _player01;
+                if (_msgBox.Controls.Find("txtPlayer01", true)[0].Text == "" || _msgBox.Controls.Find("txtPlayer01", true)[0].Text.Length > 11)
+                {
+                    MessageBox.Show("Merci de rentrer un nom d'utilisateur ayant au maximum 11 lettres", "Nom utilisateur manquant", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    _player01 = _msgBox.Controls.Find("txtPlayer01", true)[0].Text;
+                    _player02 = "Ordinateur";
+                    _msgBox.Close();
+                    _view.Text = "Morpion: Bienvenue " + _player01;
+                }
             }
+            
         }
 
-        private void LocalMulti_click(object sender, EventArgs e)
+        public void Network_click(object sender, EventArgs e)
         {
 
         }
-        private void Network_click(object sender, EventArgs e)
+        public void Rules_click(object sender, EventArgs e)
         {
 
         }
-        private void Rules_click(object sender, EventArgs e)
-        {
-
-        }
-        private void Infos_click(object sender, EventArgs e)
+        public void Infos_click(object sender, EventArgs e)
         {
 
         }
         private void topMenu()
         {
 
-            System.Windows.Forms.MenuStrip mnu;
-            System.Windows.Forms.ToolStripMenuItem mnuLocal;
-            System.Windows.Forms.ToolStripMenuItem mnuLocalSolo;
-            System.Windows.Forms.ToolStripMenuItem mnuLocalMulti;
-            System.Windows.Forms.ToolStripMenuItem mnuNetwork;
-            System.Windows.Forms.ToolStripMenuItem mnuRules;
-            System.Windows.Forms.ToolStripMenuItem mnuInfos;
+            MenuStrip mnu;
+            ToolStripMenuItem mnuLocal;
+            ToolStripMenuItem mnuLocalSolo;
+            ToolStripMenuItem mnuLocalMulti;
+            ToolStripMenuItem mnuNetwork;
+            ToolStripMenuItem mnuRules;
+            ToolStripMenuItem mnuInfos;
 
-            mnu = new System.Windows.Forms.MenuStrip();
-            mnuLocal = new System.Windows.Forms.ToolStripMenuItem();
-            mnuLocalSolo = new System.Windows.Forms.ToolStripMenuItem();
-            mnuLocalMulti = new System.Windows.Forms.ToolStripMenuItem();
-            mnuNetwork = new System.Windows.Forms.ToolStripMenuItem();
-            mnuRules = new System.Windows.Forms.ToolStripMenuItem();
-            mnuInfos = new System.Windows.Forms.ToolStripMenuItem();
+            mnu = new MenuStrip();
+            mnuLocal = new ToolStripMenuItem();
+            mnuLocalSolo = new ToolStripMenuItem();
+            mnuLocalMulti = new ToolStripMenuItem();
+            mnuNetwork = new ToolStripMenuItem();
+            mnuRules = new ToolStripMenuItem();
+            mnuInfos = new ToolStripMenuItem();
             
             mnu.SuspendLayout();
 
             //insert menus in menustrip
             mnu.Name = "mnu";
             mnu.Text = "mnu";
-            mnu.BackColor = System.Drawing.Color.Silver;
-            mnu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {   mnuLocal,
+            mnu.BackColor = Color.Silver;
+            mnu.Items.AddRange(new ToolStripItem[] {   mnuLocal,
                                                                             mnuNetwork,
                                                                             mnuRules,
                                                                             mnuInfos});
-            mnu.Location = new System.Drawing.Point(0, 0);
-            mnu.Size = new System.Drawing.Size(600, 24);
+            mnu.Location = new Point(0, 0);
+            mnu.Size = new Size(600, 24);
             mnu.TabIndex = 0;
 
             // add local menu in mnu
-            mnuLocal.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            mnuLocal.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {  mnuLocalSolo,
+            mnuLocal.BackgroundImageLayout = ImageLayout.None;
+            mnuLocal.DropDownItems.AddRange(new ToolStripItem[] {  mnuLocalSolo,
                                                                                         mnuLocalMulti});
             mnuLocal.Name = "mnuLocal";
-            mnuLocal.Size = new System.Drawing.Size(86, 20);
+            mnuLocal.Size = new Size(86, 20);
             mnuLocal.Text = "Partie Locale";
 
             //add solo button in local menu
             mnuLocalSolo.Name = "mnuLocalSolo";
-            mnuLocalSolo.Size = new System.Drawing.Size(180, 22);
+            mnuLocalSolo.Size = new Size(180, 22);
             mnuLocalSolo.Text = "En Solo";
             mnuLocalSolo.Click += LocalSolo_click;
 
             //add multijoueur button in local menu
             mnuLocalMulti.Name = "mnuLocalMulti";
-            mnuLocalMulti.Size = new System.Drawing.Size(180, 22);
+            mnuLocalMulti.Size = new Size(180, 22);
             mnuLocalMulti.Text = "En Multijoueur";
             mnuLocalMulti.Click += LocalMulti_click;
 
             //create mnuNetwork in mnu
             mnuNetwork.Name = "mnuNetwork";
-            mnuNetwork.Size = new System.Drawing.Size(120, 20);
+            mnuNetwork.Size = new Size(120, 20);
             mnuNetwork.Text = "Partie sur le Réseau";
             mnuNetwork.Click += Network_click;
 
             //create mnuRules in mnu
             mnuRules.Name = "mnuRules";
-            mnuRules.Size = new System.Drawing.Size(53, 20);
+            mnuRules.Size = new Size(53, 20);
             mnuRules.Text = "Règles";
             mnuRules.Click += Rules_click;
 
             //create mnuInfos in mnu
             mnuInfos.Name = "mnuInfos";
-            mnuInfos.Size = new System.Drawing.Size(87, 20);
+            mnuInfos.Size = new Size(87, 20);
             mnuInfos.Text = "Informations";
             mnuNetwork.Click += Infos_click;
 
@@ -167,68 +212,63 @@ namespace Morpion
 
         private void main_menu()
         {
-            System.Windows.Forms.Button cmdLocalSolo;
-            System.Windows.Forms.Button cmdLocalMulti;
-            System.Windows.Forms.Button cmdNetwork;
-            System.Windows.Forms.Button cmdRules;
-            System.Windows.Forms.Button cmdInfos;
+            Button cmdLocalSolo;
+            Button cmdLocalMulti;
+            Button cmdNetwork;
+            Button cmdRules;
+            Button cmdInfos;
 
 
-            cmdLocalSolo = new System.Windows.Forms.Button();
-            cmdLocalMulti = new System.Windows.Forms.Button();
-            cmdNetwork = new System.Windows.Forms.Button();
-            cmdRules = new System.Windows.Forms.Button();
-            cmdInfos = new System.Windows.Forms.Button();
+            cmdLocalSolo = new Button();
+            cmdLocalMulti = new Button();
+            cmdNetwork = new Button();
+            cmdRules = new Button();
+            cmdInfos = new Button();
             // 
             // cmdLocalSolo
             // 
-            cmdLocalSolo.Location = new System.Drawing.Point(0, 25);
+            cmdLocalSolo.Location = new Point(0, 25);
             cmdLocalSolo.Name = "cmdLocalSolo";
-            cmdLocalSolo.Size = new System.Drawing.Size(785, 107);
+            cmdLocalSolo.Size = new Size(785, 107);
             cmdLocalSolo.TabIndex = 0;
-            cmdLocalSolo.Text = "cmdLocalSolo";
+            cmdLocalSolo.Text = "Solo";
             cmdLocalSolo.UseVisualStyleBackColor = true;
-            cmdLocalSolo.Click += LocalSolo_click;
             // 
             // cmdLocalMulti
             // 
-            cmdLocalMulti.Location = new System.Drawing.Point(0, 132);
+            cmdLocalMulti.Location = new Point(0, 132);
             cmdLocalMulti.Name = "cmdLocalMulti";
-            cmdLocalMulti.Size = new System.Drawing.Size(785, 107);
+            cmdLocalMulti.Size = new Size(785, 107);
             cmdLocalMulti.TabIndex = 2;
-            cmdLocalMulti.Text = "cmdLocalMulti";
+            cmdLocalMulti.Text = "Multiojoueur";
             cmdLocalMulti.UseVisualStyleBackColor = true;
-            cmdLocalMulti.Click += LocalMulti_click;
             // 
             // cmdNetwork
             // 
-            cmdNetwork.Location = new System.Drawing.Point(0, 239);
+            cmdNetwork.Location = new Point(0, 239);
             cmdNetwork.Name = "cmdNetwork";
-            cmdNetwork.Size = new System.Drawing.Size(785, 107);
+            cmdNetwork.Size = new Size(785, 107);
             cmdNetwork.TabIndex = 3;
-            cmdNetwork.Text = "cmdNetwork";
+            cmdNetwork.Text = "Partie sur le Réseau";
             cmdNetwork.UseVisualStyleBackColor = true;
-            cmdNetwork.Click += Network_click;
             // 
             // cmdRules
             // 
-            cmdRules.Location = new System.Drawing.Point(0, 346);
+            cmdRules.Location = new Point(0, 346);
             cmdRules.Name = "cmdRules";
-            cmdRules.Size = new System.Drawing.Size(785, 107);
+            cmdRules.Size = new Size(785, 107);
             cmdRules.TabIndex = 4;
-            cmdRules.Text = "cmdRules";
+            cmdRules.Text = "Règles";
             cmdRules.UseVisualStyleBackColor = true;
-            cmdRules.Click += Rules_click;
             // 
             // cmdInfos
             // 
-            cmdInfos.Location = new System.Drawing.Point(0, 453);
+            cmdInfos.Location = new Point(0, 453);
             cmdInfos.Name = "cmdInfos";
-            cmdInfos.Size = new System.Drawing.Size(785, 107);
+            cmdInfos.Size = new Size(785, 107);
             cmdInfos.TabIndex = 5;
-            cmdInfos.Text = "cmdInfos";
+            cmdInfos.Text = "Informations";
             cmdInfos.UseVisualStyleBackColor = true;
-            cmdInfos.Click += Infos_click;
 
             _view.Controls.Add(cmdLocalSolo);
             _view.Controls.Add(cmdLocalMulti);
@@ -236,20 +276,123 @@ namespace Morpion
             _view.Controls.Add(cmdRules);
             _view.Controls.Add(cmdInfos);
         }
+
+        private void game_int()
+        {
+            _view.Controls.Clear();
+
+
+            Label lblPlayer01 = new Label();
+            Label lblWinP01 = new Label();
+            Label lblScoreP01 = new Label();
+            // 
+            // lblPlayer01
+            // 
+            lblPlayer01.AutoSize = true;
+            lblPlayer01.Font = new Font("Microsoft Sans Serif", 20.25F, ((FontStyle)((FontStyle.Bold | FontStyle.Italic))), GraphicsUnit.Point, ((byte)(0)));
+            lblPlayer01.Location = new Point(12, 105);
+            lblPlayer01.Name = "lblPlayer01";
+            lblPlayer01.Size = new Size(187, 31);
+            lblPlayer01.Text = "NoxCaedibux";
+            _view.Controls.Add(lblPlayer01);
+
+            // 
+            // lblWinP01
+            // 
+            lblWinP01.AutoSize = true;
+            lblWinP01.Font = new Font("Microsoft Sans Serif", 15.75F, FontStyle.Regular, GraphicsUnit.Point, ((byte)(0)));
+            lblWinP01.Location = new Point(45, 157);
+            lblWinP01.Name = "lblWinP01";
+            lblWinP01.Size = new Size(95, 25);
+            lblWinP01.Text = "Victoires";
+            _view.Controls.Add(lblWinP01);
+
+            // 
+            // lblScoreP01
+            // 
+            lblScoreP01.AutoSize = true;
+            lblScoreP01.Font = new Font("Microsoft Sans Serif", 21.75F, FontStyle.Regular, GraphicsUnit.Point, ((byte)(0)));
+            lblScoreP01.Location = new Point(66, 182);
+            lblScoreP01.Name = "lblScoreP01";
+            lblScoreP01.Size = new Size(47, 33);
+            lblScoreP01.Text = _score.ToString();
+            _view.Controls.Add(lblScoreP01);
+
+            Label lblPlayer02 = lblPlayer01;
+            Label lblWinP02 = lblWinP01;
+            Label lblScoreP02 = lblScoreP01;
+
+            lblPlayer02.Location = new Point(659, 182);
+            _view.Controls.Add(lblPlayer02);
+            lblWinP02.Location = new Point(638, 157);
+            _view.Controls.Add(lblWinP02);
+            lblScoreP02.Location = new Point(605, 105);
+            _view.Controls.Add(lblScoreP02);
+
+            PictureBox[] picCases;
+            picCases = new PictureBox[9];
+            Point location;
+            int y;
+            int x;
+            for(int i=0 ; i<10 ; i++)
+            {
+                picCases[i] = new PictureBox();
+                picCases[i].Name = "pic"+i;
+                picCases[i].Size = new System.Drawing.Size(90, 90);
+                
+                if(i == 0 || i == 2 || i == 5)
+                {
+                    x = 264;
+                }
+
+                if(i<3)
+                {
+                    y = 33;
+                    
+                }
+                else if (i<6)
+                {
+                    y = 130;
+                }
+                else
+                {
+                    y = 229;
+                }
+                picCases[i].Location = new Point(x,y);
+                x += 97;
+                _view.Controls.Add(picCases[i]);
+            }
+
+
+    }
         public void Show_interface(View view, int menus=0)
         {
             _view = view;
+            _menu = menu;
             topMenu();
-            switch(menus)
+            switch(_menu)
             {
                 case 0: // main menu
                     _viewSize = new Size(785, 560);
                     main_menu();
                     break;
+                case 1:
+                    _viewSize = new Size(820, 371);
+                    game_int();
+                    break;
                 default:
                     break;
             }
             _view.ClientSize = _viewSize;
+        }
+
+
+        public int menu
+        {
+            get
+            {
+                return _menu;
+            }
         }
     }
 }
--- CODE/Morpion/Morpion/controlor.cs
+++ CODE/Morpion/Morpion/controlor.cs
@@ -26,7 +26,26 @@ namespace Morpion
             _model.Show_interface(_view);
             _view.ResumeLayout(false);
             _view.PerformLayout();
+
+            switch(_model.menu)
+            {
+                case 0:
+                    _view.Controls.Find("cmdLocalSolo", true)[0].Click += _model.LocalSolo_click;
+                    _view.Controls.Find("cmdLocalMulti", true)[0].Click += _model.LocalMulti_click;
+                    _view.Controls.Find("cmdNetwork", true)[0].Click += _model.Network_click;
+                    _view.Controls.Find("cmdRules", true)[0].Click += _model.Rules_click;
+                    _view.Controls.Find("cmdInfos", true)[0].Click += _model.Infos_click;
+                    break;
+                default:
+                    break;
+            }
+
             End_program();
+        }
+
+        private void Controlor_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("ici");
         }
 
         private void End_program()
